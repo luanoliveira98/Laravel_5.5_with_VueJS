2e08451223f40bc09f1ed9bf95630cae42d195dc
Luan <luanoliveira17@hotmail.com>
2019-02-02 21:52:48 -0200

    class 3: TopComponent.vue

--- blog/resources/assets/js/app.js
+++ blog/resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('top-component', require('./components/Top.vue'));
 
 const app = new Vue({
     el: '#app'
