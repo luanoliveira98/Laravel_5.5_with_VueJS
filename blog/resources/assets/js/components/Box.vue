7433c743f80f97c772d41347b386fd1a8fc19164
Luan <luanoliveira17@hotmail.com>
2019-02-02 22:38:53 -0200

    class 7: Define Boxes

--- blog/resources/assets/js/components/Box.vue
+++ blog/resources/assets/js/components/Box.vue
@@ -1,22 +1,27 @@
 <template>
-    <div class="small-box bg-aqua">
+    <div class="small-box" v-bind:style="defineColor">
         <div class="inner">
-            <h3>150</h3>
+            <h3>{{ quantify }}</h3>
 
-            <p>New Orders</p>
+            <p>{{ title }}</p>
         </div>
         <div class="icon">
-            <i class="ion ion-stats-bars"></i>
+            <i v-bind:class="icon"></i>
         </div>
         <a href="#" class="small-box-footer">
-            More info <i class="fa fa-arrow-circle-right"></i>
+            See more <i class="fa fa-arrow-circle-right"></i>
         </a>
     </div>
 </template>
 
 <script>
     export default {
-        
+        props: ['quantify', 'title', 'url', 'color', 'icon'],
+        computed: {
+            defineColor:function(){
+                return "background-color: "+ this.color +" !important;";
+            }
+        }
     }
 </script>
 
@@ -31,6 +36,12 @@
         display: block;
         margin-bottom: 20px;
         box-shadow: 0 1px 1px rgba(0,0,0,0.1);
+        color: #FFF;
+    }
+
+    .small-box:hover {
+        text-decoration: none;
+        color: #f9f9f9;
     }
 
     .small-box>.inner {
@@ -69,6 +80,10 @@
         color: rgba(0,0,0,0.15);
     }
 
+    .small-box:hover .icon {
+        font-size: 95px;
+    }
+
     .small-box>.small-box-footer {
         position: relative;
         text-align: center;
@@ -81,8 +96,19 @@
         text-decoration: none;
     }
 
+    .small-box>.small-box-footer:hover {
+        color: #fff;
+        background: rgba(0,0,0,0.15);
+    }
+
     a {
         color: #3c8dbc;
+    }
+
+    a:hover, a:active, a:focus {
+        outline: none;
+        text-decoration: none;
+        color: #72afd2;
     }
 </style>
 
--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -9,20 +9,22 @@
 
                     <div class="row">
                         <div class="col-md-4">
-                            <box-component></box-component>
+                            <box-component quantify="80" title="Articles" url="#" color="orange" icon="ion ion-pie-graph">
+
+                            </box-component>
                         </div>
                         
                         <div class="col-md-4">
-                            <panel-component title="Content 2" color="orange">
-                                Content test...
-                            </panel-component>
+                            <box-component quantify="1500" title="Users" url="#" color="blue" icon="ion ion-person-stalker">
+
+                            </box-component>
                         </div>
 
                             
                         <div class="col-md-4">
-                            <panel-component title="Content 3" color="panel-info">
-                                Content test...
-                            </panel-component>
+                            <box-component quantify="3" title="Autors" url="#" color="red" icon="ion ion-person">
+
+                            </box-component>
                         </div>
                     </div>
                 </panel-component>
