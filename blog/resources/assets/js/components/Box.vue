e89ee4110d6ba384b1f88c023463f7c644da4bec
Luan <luanoliveira17@hotmail.com>
2019-02-02 23:01:00 -0200

    class9: ArticlesController and Articles Routes

--- blog/resources/assets/js/components/Box.vue
+++ blog/resources/assets/js/components/Box.vue
@@ -8,7 +8,7 @@
         <div class="icon">
             <i v-bind:class="icon"></i>
         </div>
-        <a href="#" class="small-box-footer">
+        <a v-bind:href="url" class="small-box-footer">
             See more <i class="fa fa-arrow-circle-right"></i>
         </a>
     </div>
