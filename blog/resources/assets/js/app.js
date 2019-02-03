cd25090688db42f2fbe71e1851d8fe0b498bc1e7
Luan <luanoliveira17@hotmail.com>
2019-02-02 22:02:06 -0200

    class 4: PanelComponent.vue

--- blog/resources/assets/js/app.js
+++ blog/resources/assets/js/app.js
@@ -15,8 +15,8 @@ window.Vue = require('vue');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
 Vue.component('top-component', require('./components/Top.vue'));
+Vue.component('panel-component', require('./components/Panel.vue'));
 
 const app = new Vue({
     el: '#app'
--- blog/resources/views/home.blade.php
+++ blog/resources/views/home.blade.php
@@ -1,5 +1,34 @@
 @extends('layouts.app')
 
 @section('content')
-    <example-component></example-component> 
+    <div class="container">
+        <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+                <panel-component title="Dashboard">
+                    Content test...
+
+                    <div class="row">
+                        <div class="col-md-4">
+                            <panel-component title="Content 1">
+                                Content test...
+                            </panel-component>
+                        </div>
+                        
+                        <div class="col-md-4">
+                            <panel-component title="Content 2">
+                                Content test...
+                            </panel-component>
+                        </div>
+
+                            
+                        <div class="col-md-4">
+                            <panel-component title="Content 3">
+                                Content test...
+                            </panel-component>
+                        </div>
+                    </div>
+                </panel-component>
+            </div>
+        </div>
+    </div>
 @endsection
